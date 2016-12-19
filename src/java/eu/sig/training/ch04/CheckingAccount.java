1b26981a35915e022716a3540fe912e774ee64df
Rob van der Leek <r.vanderleek@sig.eu>
2016-12-19 16:10:06 +0100

    Increase code duplication

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -2,6 +2,8 @@ package eu.sig.training.ch04;
 
 // tag::CheckingAccount[]
 public class CheckingAccount {
+    private static final float INTEREST_PERCENTAGE = 0.01f;
+    private Money balance = new Money();
     private int transferLimit = 100;
 
     public Transfer makeTransfer(String counterAccount, Money amount)
@@ -24,6 +26,15 @@ public class CheckingAccount {
             return result;
         } else {
             throw new BusinessException("Invalid account number!");
+        }
+    }
+
+    public void addInterest() {
+        Money interest = balance.multiply(INTEREST_PERCENTAGE);
+        if (interest.greaterThan(0)) {
+            balance.add(interest);
+        } else {
+            balance.substract(interest);
         }
     }
 }
--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -2,7 +2,9 @@ package eu.sig.training.ch04;
 
 // tag::SavingsAccount[]
 public class SavingsAccount {
-    CheckingAccount registeredCounterAccount;
+    private static final float INTEREST_PERCENTAGE = 0.04f;
+    private Money balance = new Money();
+    private CheckingAccount registeredCounterAccount;
 
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
@@ -26,6 +28,15 @@ public class SavingsAccount {
             }
         } else {
             throw new BusinessException("Invalid account number!!");
+        }
+    }
+
+    public void addInterest() {
+        Money interest = balance.multiply(INTEREST_PERCENTAGE);
+        if (interest.greaterThan(0)) {
+            balance.add(interest);
+        } else {
+            balance.substract(interest);
         }
     }
 }
